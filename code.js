7cca09e79b5ab09d296aecaa9aa49e3781af5431
kyo3479 <98404450+kyo3479@users.noreply.github.com>
2024-02-27 01:03:05 +0900

    Add files via upload

---
+++ code.js
@@ -0,0 +1,3 @@
+gsap.fromTo("#verline1",{x:700,opacity:0},{x:840,ease:"circ.out",duration : 1,opacity:1});
+gsap.fromTo("#verline2",{x:1180,y:-1900,opacity:0},{x:1040,ease:"circ.out",duration : 1,opacity:1});
+gsap.to("#text1",{y:-200,ease:"expo.out",duration:1.5,opacity:1})
